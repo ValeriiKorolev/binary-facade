04b844a2e1221aa22fad9be9eb9af845c2a1fd0a
ValeriiKorolev <kingvv63@gmail.com>
2022-02-17 23:14:27 +0300

    Version 1

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,22 @@
+public class BinOps {
+
+    public String sum(String a, String b) {
+        int result = Integer.parseInt(a, 2) + Integer.parseInt(b, 2);
+        return Integer.toBinaryString(result);
+    }
+
+    public String mult(String a, String b) {
+        int result = Integer.parseInt(a, 2) * Integer.parseInt(b, 2);
+        return Integer.toBinaryString(result);
+    }
+
+    public String sub(String a, String b) {
+        int result = Integer.parseInt(a, 2) - Integer.parseInt(b, 2);
+        return Integer.toBinaryString(result);
+    }
+
+    public String div(String a, String b) {
+        int result = Integer.parseInt(a, 2) / Integer.parseInt(b, 2);
+        return Integer.toBinaryString(result);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.div("1000", "10"));
+        System.out.println(bins.mult("1000", "10"));
+        System.out.println(bins.sum("1000", "10"));
+        System.out.println(bins.sub("1000", "10"));
+    }
+}
